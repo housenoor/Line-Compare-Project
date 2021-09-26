96044d32c9d19eda4a72ec4e1e552109a8669042
Srikanth <srikanthh994@gmail.com>
2021-09-26 13:44:08 +0530

    Added:Equating of two lines

--- UC_LineComparison.java
+++ UC_LineComparison.java
@@ -6,17 +6,37 @@ public class UC_LineComparison {
 	public static void main(String[] args) {
         
 		Scanner sc= new Scanner((System.in));
-		System.out.println("Enter the x-axis co-ordinates");
-        int x1=sc.nextInt();
-        int x2=sc.nextInt();
-        System.out.println("Enter the Y-axis Co-ordinates");
-        int y1=sc.nextInt();
-        int y2=sc.nextInt();
 
-        double lineLength=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-        System.out.println("The length of Line is" + lineLength);
-    }
+        // assigning the lin1 X&Y coordinates
+        System.out.println("Enter the X & Y co-ordinates of line1");
+        System.out.println("x1= ");
+        int x1 = sc.nextInt();
+        System.out.println("y1= ");
+        int y1= sc.nextInt();
+        System.out.println("x2= ");
+        int x2 = sc.nextInt();
+        System.out.println("y2= ");
+        int y2 = sc.nextInt();
+
+
+        double lineLength1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+
+        // assigning the X &Y coordinates
+        System.out.println("Enter the  X & Y co-ordinates line2");
+        System.out.println("x3= ");
+        int x3 = sc.nextInt();
+        System.out.println("y3= ");
+        int y3 = sc.nextInt();
+        System.out.println("x4= ");
+        int x4 = sc.nextInt();
+        System.out.println("y4= ");
+        int y4 = sc.nextInt();
+        double lineLength2 = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3)); 
+	System.out.println("The length of Line1 is" + lineLength1);
+ 	System.out.println("The length of Line2 is" + lineLength2);
 		
+	}
 		
 	}
 
