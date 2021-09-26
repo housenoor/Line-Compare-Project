65c08f8599c2620879635149cd4211b405890d51
Srikanth <srikanthh994@gmail.com>
2021-09-26 09:18:34 +0530

    Added:Calculation of Coordination length

--- UC_LineComparison.java
+++ UC_LineComparison.java
@@ -1,7 +1,22 @@
 package linecompare;
+import java.util.Scanner;
 
 public class UC_LineComparison {
+	
 	public static void main(String[] args) {
-        System.out.println("Welcome to the Line Comparision Program");
+        
+		Scanner sc= new Scanner((System.in));
+		System.out.println("Enter the x-axis co-ordinates");
+        int x1=sc.nextInt();
+        int x2=sc.nextInt();
+        System.out.println("Enter the Y-axis Co-ordinates");
+        int y1=sc.nextInt();
+        int y2=sc.nextInt();
+
+        double lineLength=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+        System.out.println("The length of Line is" + lineLength);
+    }
+		
+		
 	}
-}
+
