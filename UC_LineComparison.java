384bff32d9e55b10eb1945e1a2050fb4b80581ba
Srikanth <srikanthh994@gmail.com>
2021-09-26 14:32:48 +0530

    Modified:Compared less or greater and equal or not

--- UC_LineComparison.java
+++ UC_LineComparison.java
@@ -12,17 +12,15 @@ public class UC_LineComparison {
         System.out.println("x1= ");
         int x1 = sc.nextInt();
         System.out.println("y1= ");
-        int y1= sc.nextInt();
+        int y1 = sc.nextInt();
         System.out.println("x2= ");
         int x2 = sc.nextInt();
         System.out.println("y2= ");
         int y2 = sc.nextInt();
-
-
         double lineLength1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 
 
-        // assigning the X &Y coordinates
+        // assigning the X &Y coordinates of line2
         System.out.println("Enter the  X & Y co-ordinates line2");
         System.out.println("x3= ");
         int x3 = sc.nextInt();
@@ -33,10 +31,19 @@ public class UC_LineComparison {
         System.out.println("y4= ");
         int y4 = sc.nextInt();
         double lineLength2 = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3)); 
-	System.out.println("The length of Line1 is" + lineLength1);
- 	System.out.println("The length of Line2 is" + lineLength2);
-		
-	}
-		
-	}
 
+        System.out.println("The length of Line1 is" + lineLength1);
+        System.out.println("The length of line2 is " + lineLength2);
+
+        //computation
+        if (lineLength1 == lineLength2) {
+            System.out.println("Two Lines are Equal");
+        }
+            else if (lineLength1 < lineLength2) {
+        	
+            System.out.println("The line1 is less than line2");
+        } else {
+            System.out.println("Two Lines are Not Equal");
+        }
+	}
+}
